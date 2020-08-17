c3cd7ad5058928959427b8c0f9e6f3686a256a23
Suman Palikhe <Suman.Palikhe@iriworldwide.com.au>
2020-08-18 07:09:35 +1000

    init

--- src/WooliesTechChallenge/Controllers/TechChallengeController.cs
+++ src/WooliesTechChallenge/Controllers/TechChallengeController.cs
@@ -11,6 +11,10 @@ namespace WooliesTechChallenge.Controllers
     [Route("api/[controller]")]
     public class TechChallengeController : ControllerBase
     {
-
+        [HttpGet("user")]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Suman Palikhe", token = "fd627ca4-707f-4eb3-9b33-6a03264e09ed" });
+        }
     }
 }
